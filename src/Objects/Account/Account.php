5a05cff457720f5c32c7fc54aa4602be47b4168e
Mark Robertshaw <mark@oxil.co.uk>
2019-04-10 14:07:18 +0100

    account objects

---
+++ src/Objects/Account/Account.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace Kinicart\Objects\Account;
+
+
+use Kinikit\Persistence\UPF\Object\ActiveRecord;
+
+/**
+ * Main account business object.  Users can belong to one or more accounts.
+ *
+ * Class Account
+ */
+class Account extends ActiveRecord {
+
+
+    /**
+     * Auto increment id.
+     *
+     * @var integer
+     */
+    protected $id;
+
+
+    /**
+     * The account name - optional
+     *
+     * @var string
+     */
+    private $name;
+
+
+    /**
+     * Boolean as to whether or not
+     *
+     * @var boolean
+     */
+    private $subAccountsEnabled;
+
+
+    /**
+     * Id of a parent account if this is a sub account
+     *
+     * @var integer
+     */
+    private $parentAccountId;
+
+
+    /**
+     * @return int
+     */
+    public function getId() {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName() {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName($name) {
+        $this->name = $name;
+    }
+
+    /**
+     * @return bool
+     */
+    public function getSubAccountsEnabled() {
+        return $this->subAccountsEnabled;
+    }
+
+    /**
+     * @param bool $subAccountsEnabled
+     */
+    public function setSubAccountsEnabled($subAccountsEnabled) {
+        $this->subAccountsEnabled = $subAccountsEnabled;
+    }
+
+    /**
+     * @return int
+     */
+    public function getParentAccountId() {
+        return $this->parentAccountId;
+    }
+
+    /**
+     * @param int $parentAccountId
+     */
+    public function setParentAccountId($parentAccountId) {
+        $this->parentAccountId = $parentAccountId;
+    }
+
+
+}
