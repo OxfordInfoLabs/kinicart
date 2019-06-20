0da93ba96f2c72b8c410a4ab27abac5d3cf53855
Mark Robertshaw <mark@oxil.co.uk>
2019-06-20 13:18:32 +0100

    New Account Service.

--- src/Services/Account/AccountService.php
+++ src/Services/Account/AccountService.php
@@ -4,6 +4,20 @@
 namespace Kinicart\Services\Account;
 
 
+use Kinicart\Objects\Account\Account;
+use Kinicart\Objects\Account\AccountSummary;
+
 class AccountService {
+
+    /**
+     * Get an account summary, default to the logged in account.
+     *
+     * @param string $id
+     * @return AccountSummary
+     */
+    public function getAccountSummary($id = Account::LOGGED_IN_ACCOUNT) {
+        $accountSummary =  AccountSummary::fetch($id);
+        return $accountSummary;
+    }
 
 }
