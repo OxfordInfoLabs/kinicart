af6e297cb38a00b718587b49f4f1352416458d0c
Mark Robertshaw <mark@oxil.co.uk>
2019-09-23 16:55:52 +0100

    Old Kinicart logic ripped out.

--- src/DB/views.sql
+++
@@ -1,7 +0,0 @@
-DROP VIEW IF EXISTS kc_vw_user_role;
-
-CREATE VIEW kc_vw_user_role AS
-    SELECT uar.*, r.privileges, a.status account_status
-        FROM kc_user_role uar
-        LEFT JOIN kc_role r ON uar.role_id = r.id
-        LEFT JOIN kc_account a ON uar.scope = 'ACCOUNT' AND uar.scope_id = a.account_id;
