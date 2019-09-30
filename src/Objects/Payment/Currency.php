c770805f89b3ed4e7dee0a33735d2bebb2946d15
Mark Robertshaw <mark@oxil.co.uk>
2019-09-30 15:15:37 +0100

    New cart logic started

---
+++ src/Objects/Payment/Currency.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Kinicart\Objects\Payment;
+
+use Kinikit\Persistence\ORM\ActiveRecord;
+
+/**
+ * Currency class - encodes the in use currencies for the system
+ *
+ * Class Currency
+ *
+ * @table kc_currency
+ */
+class Currency extends ActiveRecord {
+
+    /**
+     * The 3 character code for the currency
+     *
+     * @primaryKey
+     * @var string
+     */
+    private $code;
+
+
+    /**
+     * Friendly description for this currency (e.g. US Dollars)
+     *
+     * @var string
+     */
+    private $description;
+
+
+    /**
+     * The exchange rate from the system base rate.
+     *
+     * @var float
+     */
+    private $exchangeRateFromBase;
+
+
+    /**
+     * @return string
+     */
+    public function getCode() {
+        return $this->code;
+    }
+
+    /**
+     * @param string $code
+     */
+    public function setCode($code) {
+        $this->code = $code;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription() {
+        return $this->description;
+    }
+
+    /**
+     * @param string $description
+     */
+    public function setDescription($description) {
+        $this->description = $description;
+    }
+
+    /**
+     * @return float
+     */
+    public function getExchangeRateFromBase() {
+        return $this->exchangeRateFromBase;
+    }
+
+    /**
+     * @param float $exchangeRateFromBase
+     */
+    public function setExchangeRateFromBase($exchangeRateFromBase) {
+        $this->exchangeRateFromBase = $exchangeRateFromBase;
+    }
+
+
+}
---
+++ src/Objects/Pricing/Tier.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace Kinicart\Objects\Pricing;
+
+use Kinikit\Persistence\ORM\ActiveRecord;
+
+
+/**
+ * Pricing tier - these are typically assigned to accounts
+ *
+ * Class Tier
+ * @package Kinicart\Objects\Pricing
+ *
+ * @table kc_tier
+ */
+class Tier extends ActiveRecord {
+
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $name;
+
+
+    /**
+     * @var float
+     */
+    private $defaultPriceMultiplier;
+
+
+    /**
+     * @var boolean
+     */
+    private $defaultTier;
+
+    /**
+     * @var integer
+     */
+    private $upgradeOrder;
+
+
+    /**
+     * @return int
+     */
+    public function getId() {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId($id) {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName() {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName($name) {
+        $this->name = $name;
+    }
+
+    /**
+     * @return float
+     */
+    public function getDefaultPriceMultiplier() {
+        return $this->defaultPriceMultiplier;
+    }
+
+    /**
+     * @param float $defaultPriceMultiplier
+     */
+    public function setDefaultPriceMultiplier($defaultPriceMultiplier) {
+        $this->defaultPriceMultiplier = $defaultPriceMultiplier;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isDefaultTier() {
+        return $this->defaultTier;
+    }
+
+    /**
+     * @param bool $defaultTier
+     */
+    public function setDefaultTier($defaultTier) {
+        $this->defaultTier = $defaultTier;
+    }
+
+    /**
+     * @return int
+     */
+    public function getUpgradeOrder() {
+        return $this->upgradeOrder;
+    }
+
+    /**
+     * @param int $upgradeOrder
+     */
+    public function setUpgradeOrder($upgradeOrder) {
+        $this->upgradeOrder = $upgradeOrder;
+    }
+
+
+}
