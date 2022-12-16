5c1d9fdc1d1106f66e0d0691c2ea1e7d25708d58
mark@oxil.co.uk <mark@oxil.co.uk>
2022-12-16 13:03:10 +0000

    Removed old code

--- php/src/Objects/Payment/Payment.php
+++ php/src/Objects/Payment/Payment.php
@@ -3,16 +3,22 @@
 
 namespace Kinicart\Objects\Payment;
 
-
+/**
+ * Payment base interface
+ *
+ * @implementation stripe \Kinicart\Objects\Payment\Stripe\StripePayment
+ */
 interface Payment {
 
     /**
      * Function used to charge the attached payment method for the amount specified
      *
-     * @param $amount
-     * @param $currency
+     * @param float $amount
+     * @param string $currency
+     * @param mixed $paymentData
+     *
      * @return mixed
      */
-    public function charge($amount, $currency);
+    public function charge($amount, $currency, $paymentData = null);
 
 }
